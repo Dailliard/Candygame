550d223cadd38e39a9aae94ee31a50fdc6e9b7a1
Dailliard <maxence.dailliard@edu.ece.fr>
2025-11-25 23:46:41 +0100

    test 25/11 2

--- visual.c
+++ visual.c
@@ -25,34 +25,3 @@ void menu(void) {
   }
 }
 
-void rules(void) {
-    system("cls");
-    printf("Here are the rules of the Candy Game...\n");
-    // Add more detailed rules here
-    printf("Press any key to return to the menu.\n");
-    getch();
-    menu();
-}
-void leave(void) {
-    system("cls");
-    printf("Thank you for playing the Candy Game! Goodbye!\n");
-    exit(0);
-}
-void NewGame(void) {
-    system("cls");
-    printf("Starting a new game...\n");
-    // Initialize game state here
-    printf("Press any key to return to the menu.\n");
-    getch();
-    menu();
-}
-
-void Chargegame(void) {
-    system("cls");
-    printf("Loading a saved game...\n");
-    // Load game state here
-    printf("Press any key to return to the menu.\n");
-    getch();
-    menu();
-}
-
