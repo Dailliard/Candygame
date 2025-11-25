d114ad3979dcb9b32d01b8cf1e93e80dba1dfda1
Dailliard <maxence.dailliard@edu.ece.fr>
2025-11-25 23:44:06 +0100

    test 25/11

--- visual.c
+++ visual.c
@@ -1,8 +1,58 @@
 #include "visual.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
 
 void menu(void) {
-    printf("Menu function called.\n");
+  system("cls");
+  int choice;
+  printf("Welcome to the Candy Game !\n");
+  printf("1. Rules\n");
+  printf("2. New Game\n");  
+  printf("3. Load Game\n");
+  printf("4. Exit\n");
+  scanf("%d",choice);  
+  if (choice == 1) {
+      rules();
+  } else if (choice == 2) {
+      NewGame();
+  } else if (choice == 3) {
+      Chargegame();
+  } else {
+      leave();
+  }
+}
+
+void rules(void) {
+    system("cls");
+    printf("Here are the rules of the Candy Game...\n");
+    // Add more detailed rules here
+    printf("Press any key to return to the menu.\n");
+    getch();
+    menu();
+}
+void leave(void) {
+    system("cls");
+    printf("Thank you for playing the Candy Game! Goodbye!\n");
+    exit(0);
+}
+void NewGame(void) {
+    system("cls");
+    printf("Starting a new game...\n");
+    // Initialize game state here
+    printf("Press any key to return to the menu.\n");
+    getch();
+    menu();
+}
+
+void Chargegame(void) {
+    system("cls");
+    printf("Loading a saved game...\n");
+    // Load game state here
+    printf("Press any key to return to the menu.\n");
+    getch();
+    menu();
 }
 
