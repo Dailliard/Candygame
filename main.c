09930c8d22cc403e1fe1ab475c280c821efcf1f4
Dailliard <maxence.dailliard@edu.ece.fr>
2025-12-03 20:44:33 +0100

    Menu - Rules - 03/12/2025

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include "visual.h"
 
 #include <stdio.h>
+#include <windows.h>
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
     menu();
     return 0;
 }
--- visual.c
+++ visual.c
@@ -5,23 +5,36 @@
 #include <conio.h>
 #include <windows.h>
 
+void rules(void){
+    char action;
+    do{
+        if(action == ' '){
+            break;
+        }
+        system("cls");
+        printf("Règles du jeu:\n");
+    }while(action = getch());
+}
+
 void menu(void) {
-  system("cls");
-  int choice;
-  printf("Welcome to the Candy Game !\n");
-  printf("1. Rules\n");
-  printf("2. New Game\n");  
-  printf("3. Load Game\n");
-  printf("4. Exit\n");
-  scanf("%d",choice);  
-  if (choice == 1) {
-      rules();
-  } else if (choice == 2) {
-      NewGame();
-  } else if (choice == 3) {
-      Chargegame();
-  } else {
-      leave();
-  }
+    char action;
+    do{
+        if(action == '4'){
+            break;
+        }else if(action == '3'){
+            rules();
+        }else if(action == '2'){
+            //load_game();
+        }else if(action == '1'){
+            //new_game();
+        }
+        system("cls");
+        int choice;
+        printf("ECE HEROES\n");
+        printf("1. 🎮 Nouvelle Partie\n");
+        printf("2. ⏯️  Reprendre une partie\n");  
+        printf("3. 📖 Lire les règles du jeu\n");
+        printf("4. 🔚 Quitter\n");
+    }while(action = getch());
 }
 
