6009d7da09b1860322d83d6eec967d62629ca311
Dailliard <maxence.dailliard@edu.ece.fr>
2025-12-18 17:02:54 +0100

    coups

--- game.c
+++ game.c
@@ -75,7 +75,7 @@ void partie(Donnees *data){
             if(sel == 0)sel=1;
             else sel=0;
         }
-        if (action == 'p')break;
+        if (data->coups==0)break;
         recherche_formes(tab, data);
         viewmap(tab, y, x, sel);
         
